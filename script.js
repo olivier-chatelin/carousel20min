9bd28081260031cb0487d947360b8be6b1f690b1
Olivier Chatelin <olivier.chatelin@gmail.com>
2021-10-06 22:05:04 +0200

    20 min

---
+++ script.js
@@ -0,0 +1,32 @@
+const buttons = document.getElementsByClassName('slide-button');
+const band = document.getElementById('band');
+const nbImage = 5;
+let leftOffset = 0;
+let index = 1;
+
+for(let i = 0; i < buttons.length; i ++) {
+    buttons[i].addEventListener('click', function(){
+        if (i === 1) {
+            index ++;
+            if (index > nbImage) {
+                index = 1;
+                leftOffset = 0;
+            } else {
+                leftOffset -=502;
+            }
+        } else{
+            index --;
+            if (index < 1) {
+                index = nbImage;
+                leftOffset = - (nbImage-1) * 502;
+            } else {
+                leftOffset += 502;
+            }
+        }
+        band.style.marginLeft = leftOffset + "px";
+        console.log("index:" + index);
+        console.log("offset:" + leftOffset);
+
+    })
+
+}
